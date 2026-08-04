c00042d9ad65a9096e9fa00211e93dab4affd4a9
Ashwini Burgute <ashwiniburgute9@gmail.com>
2026-08-04 14:25:09 +0530

    Database Initial

--- database/db.py
+++ database/db.py
@@ -1,21 +1,17 @@
-import os
 from pymongo import MongoClient
+import os
 
 MONGO_URI = os.getenv(
     "MONGO_URI",
-    "mongodb://mongo:27017"
+    "mongodb://localhost:27017"
 )
 
 client = MongoClient(MONGO_URI)
 
 db = client["attendance_db"]
 
-users = db["users"]
-
 students = db["students"]
-
 teachers = db["teachers"]
-
+users = db["users"]
 attendance = db["attendance"]
-
 leave_requests = db["leave_requests"]
